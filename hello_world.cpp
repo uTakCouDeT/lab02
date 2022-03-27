6679dec336e86b47215e2e9dfc5666a3771fabd1
uTakCouDeT <91888072+uTakCouDeT@users.noreply.github.com>
2022-03-27 21:25:50 +0000

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,6 +6,6 @@ int main(){
    char* name = "";
    std::cout << "Your name is: ";
    std::cin >> name;
-   std::cout << "Hello world from" << name;
+   std::cout << "Hello world from" << name << std::endl;
    return 0;
 }
