6d63dd8e972315f02333dedc684fbcdecea6cdbd
uTakCouDeT <b20030021@gmail.com>
2022-03-27 23:26:38 +0300

    New branch without using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
    char* name = "";
-   cout << "Your name is: ";
-   cin >> name;
-   cout << "Hello world from" << name << endl;
+   std::cout << "Your name is: ";
+   std::cin >> name;
+   std::cout << "Hello world from" << name;
    return 0;
 }
