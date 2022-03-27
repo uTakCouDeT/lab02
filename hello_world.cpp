313a446ab1c0e82228a9a82a811d9f260dd9f5ac
uTakCouDeT <b20030021@gmail.com>
2022-03-27 23:55:47 +0300

    New branch with new code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,13 @@
 
 // Забыл удалть using namespace std;
 
-int main(){
-   char* name = "";
-   std::cout << "Your name is: ";
-   std::cin >> name;
-   std::cout << "Hello world from" << name;
-   return 0;
+int
+main()
+{
+  char* name = "";
+  std::cout << "Your name is: ";
+  std::cin >> name;
+  std::cout << "Hello world from" << name;
+  return 0;
 }
+
